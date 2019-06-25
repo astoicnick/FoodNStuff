8092a07121b2608783bc68c3d5922b76d5fef5b4
perrynicholas <perry184@purdue.edu>
2019-06-25 11:17:28 -0400

    Set up database

---
+++ FoodNStuff.MVC/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FoodNStuff.MVC.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        //public string Fullname => $"{FirstName} {LastName}";
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+        
+
+    }
+}
